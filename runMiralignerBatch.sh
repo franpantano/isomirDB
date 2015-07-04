2e7af2ce0bb97c5803dd2057d659dcee100ee9af
Francisco Pantano <franpantano@gmail.com>
2015-07-04 18:39:40 +0200

    checking if file has extension .fa

--- runMiralignerBatch.sh
+++ runMiralignerBatch.sh
@@ -5,22 +5,27 @@ FILES=$1*
 
 for f in $FILES
 do
-  echo "Processing $f file..."
+  #echo "Processing $f file..."
   # take action on each file. $f store current file name
   
   filename=$(basename "$f")
   extension="${filename##*.}"
   filename="${filename%.*}"
+  extok="fa"
   
-  arrFN=(${filename//_/ })
+  if [ "$extension" == "$extok" ]
+  then
+    arrFN=(${filename//_/ })
   
-  ftype=${arrFN[-1]}
+    ftype=${arrFN[-1]}
   
-  #echo "${arrFN[-1]}"
+    #echo "${arrFN[-1]}"
   
-  #echo $ftype
+    #echo $ftype
   
-  #echo "Processing $filename file..."
+    echo "Processing $filename file with extension $extension..."
 
-  java -jar miraligner.jar -sub 1 -trim 3 -add 3 -s $ftype -i $f -db DB  -o $2$filename
+    java -jar miraligner.jar -sub 1 -trim 3 -add 3 -s $ftype -i $f -db DB  -o $2$filename
+
+  fi
 done 
